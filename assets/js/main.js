4aeedb407b0ff7ba53c489f463c0e28aa02eafa9
Chris Herzig <chrisherzig@msn.com>
2025-03-18 17:09:55 -0400

    Button fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -170,7 +170,7 @@ document.addEventListener("DOMContentLoaded", function () {
       const templateID = "template_6ieshmg";
       // Uncomment this for working demo
 
-       emailjs.sendForm(service_46ntgno, template_6ieshmg, this).then(
+       emailjs.sendForm(serviceID, templateID, this).then(
          () => {
            btn.value = "Send Email";
            Swal.fire("Message Sent Successfully", "", "success");
